07c1698d9af47ea76c705c2f07b101a0ae8cf1f9
nativraz1 <nativraz1@gmail.com>
2024-03-21 14:13:11 +0200

    Initial Branch Commit

--- server/db.go
+++ server/db.go
@@ -5,67 +5,44 @@ import (
 	"time"
 
 	ppb "github.com/TekClinic/Appointments-MicroService/appointments_protobuf"
-	sf "github.com/sa-/slicefunk"
 	"github.com/uptrace/bun"
 )
 
 type Appointment struct {
 	ID						int `bun:",pk,autoincrement"`
-	PatientID				int
-	DoctorID				int
+	PatientId				int
+	DoctorId				int
 	StartTime				time.Time
 	EndTime					time.Time
 	ApprovedByPatient		bool
 	Visited					bool				
 }
 
-
-func (personalId PersonalId) toGRPC() *ppb.Patient_PersonalId {
-	return &ppb.Patient_PersonalId{
-		Id:   personalId.ID,
-		Type: personalId.Type,
-	}
-}
-
-func (contact EmergencyContact) toGRPC() *ppb.Patient_EmergencyContact {
-	return &ppb.Patient_EmergencyContact{
-		Name:      contact.Name,
-		Closeness: contact.Closeness,
-		Phone:     contact.Phone,
-	}
-}
-
-func createGRPCDate(time time.Time) *ppb.Patient_Date {
-	return &ppb.Patient_Date{
+func createGRPCDate(time time.Time) *ppb.Time {
+	return &ppb.Time{
 		Day:   int32(time.Day()),
 		Month: int32(time.Month()),
 		Year:  int32(time.Year()),
+		Hour:	int32(time.Hour()),
+		Minute: int32(time.Minute()),
 	}
 }
 
-func (patient Patient) toGRPC() *ppb.Patient {
-	EmergencyContacts := sf.Map(patient.EmergencyContacts,
-		func(contact *EmergencyContact) *ppb.Patient_EmergencyContact { return contact.toGRPC() })
-	return &ppb.Patient{
-		Id:                int64(patient.ID),
-		Active:            patient.Active,
-		Name:              patient.Name,
-		PersonalId:        patient.PersonalId.toGRPC(),
-		Gender:            patient.Gender,
-		PhoneNumber:       patient.PhoneNumber,
-		Languages:         patient.Languages,
-		BirthDate:         createGRPCDate(patient.BirthDate),
-		Age:               int32(time.Now().Year() - patient.BirthDate.Year()),
-		ReferredBy:        patient.ReferredBy,
-		EmergencyContacts: EmergencyContacts,
-		SpecialNote:       patient.SpecialNote,
+func (appointment Appointment) toGRPC() *ppb.Appointment {
+	return &ppb.Appointment{
+		Id:              	    int64(appointment.ID),
+		PatientId:        	 	int64(appointment.PatientId),
+		DoctorId:               int64(appointment.DoctorId),
+		StartTime:         	    createGRPCDate(appointment.StartTime),
+		EndTime:       			createGRPCDate(appointment.EndTime),
+		ApprovedByPatient:      appointment.ApprovedByPatient,
+		Visited: 				appointment.Visited,
 	}
 }
 
 func createSchemaIfNotExists(ctx context.Context, db *bun.DB) error {
 	models := []interface{}{
-		(*Patient)(nil),
-		(*EmergencyContact)(nil),
+		(*Appointment)(nil),
 	}
 
 	for _, model := range models {
